ad7f5fc45139d7e3b31a9719495f7a40ee6acf81
Alexander <alx925@gmail.com>
2021-02-15 22:27:12 +0300

    Homework-8

---
+++ Урок 8. Progressive Web App/static_src/containers/ChatContainer/ChatContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Chat } from '../../components/Chat/Chat';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { sendMessage } from '../../store/chatActions';
+
+export const ROBOT = 'Robot';
+
+const mapStateToProps = (store, props) => {
+  const { id } = props.match.params;
+  const chats = id && store.chats.chats ? store.chats.chats : {};
+
+  return {
+    isLoading: store.chats.isLoading,
+    error: store.chats.error,
+    messages: chats[id] ? chats[id].messages : undefined,
+  };
+};
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      sendMessage,
+    },
+    dispatch
+  );
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const { id } = ownProps.match.params;
+
+  const onSendMessage = ({ name, content }) => {
+    dispatchProps.sendMessage(id, name, content);
+  };
+
+  return {
+    isLoading: stateProps.isLoading,
+    error: stateProps.error,
+    messages: stateProps.messages,
+    onSendMessage,
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Chat);
